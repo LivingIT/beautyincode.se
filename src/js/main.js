3f95a84ca4529e469575a4771e57b1e07d0bda82
mknoos <mknoos@users.noreply.github.com>
2020-04-09 11:22:08 +0000

    Automated deployment: Thu Apr  9 11:22:08 UTC 2020 c18dc124a00c3e3e1af3f091bf7127058a39cdb0

--- src/js/main.js
+++ src/js/main.js
@@ -67,7 +67,7 @@ if (didCutTheMustard) {
   document.querySelector('html').classList.add('js');
 
   // Start the countdown
-  countdown('2020/03/07 09:00:00');
+  countdown('2021/03/06 09:00:00');
 }
 
 function scrollToTop() {
