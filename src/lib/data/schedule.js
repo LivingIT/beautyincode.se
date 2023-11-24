39fe14dee55914c874d71e36741047b414fafc67
Fredrik Frodlund <fredrik@frodlund.se>
2023-11-24 12:08:59 +0100

    feat: implement basic version of schedule
    TODO: handle HTML or Markdown in description

---
+++ src/lib/data/schedule.js
@@ -0,0 +1,28 @@
+export const scheduleData = [
+  {
+    timeslot: '08:15 - 09:00',
+    name: 'The doors open',
+    image: '/images/welcome.jpg',
+    description: 'Coffee and tea will be served.'
+  },
+  {
+    timeslot: '09:00 - 10:00',
+    name: 'Roy Osherove',
+    title: 'The good, bad, and beautiful of writing blockchain-based smart contracts',
+    image: '/images/speakers/roy-osherove.jpg',
+    bio: `Roy Osherove is the founder of Legato - a decentralized licensing platform.
+          He is also the author of "The art of Unit Testing" and "Elastic Leadership".
+          Roy trains and speaks worldwide on engineering practices, continuous
+          delivery, Coding in Web3, TDD, and more.
+          
+          You can find out more at [osherove.com](https://osherove.com).`,
+    description: `Roy walks through the basics of what solidity smart contracts look like and
+                  what type of patterns are enabled through them.
+                  
+                  We also discuss what practices become extra important(hint: testing,
+                  auditing), what practices become problematic (hint: continuous delivery),
+                  and what design patterns become problematic (hint: CRUD).
+                  
+                  **Check out his masterclass "[Elastic, agile and adaptive leadership](/masterclass/)" on Friday march 3rd!**`
+  }
+];
---
+++ src/lib/utils/slugify.js
@@ -0,0 +1,13 @@
+/** @param {string} string */
+const slugify = (string) => {
+  return String(string)
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
+    .trim() // trim leading or trailing whitespace
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, ''); // remove non-alphanumeric characters
+};
+
+export default slugify;
