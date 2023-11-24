18e94e7f2fed02f3d540c4fcf107031c9ec307c8
Fredrik Frodlund <fredrik@frodlund.se>
2023-11-24 11:39:09 +0100

    feat(components): add content-block component (#242)

---
+++ src/lib/components/ContentBlock.svelte
@@ -0,0 +1,13 @@
+<script>
+  /** @type {Theme} */
+  export let theme;
+
+  /** @type {string | null} */
+  export let id = null;
+</script>
+
+<div class="content-block {theme}" id={id || null}>
+  <div class="landmark-content">
+    <slot />
+  </div>
+</div>
---
+++ src/lib/types/themes.d.ts
@@ -0,0 +1,16 @@
+// themes.d.ts
+
+/**
+ * Represents the allowed themes for the component.
+ * Each theme corresponds to a specific set of styles.
+ */
+declare global {
+  type Theme =
+    | 'theme-1' // Description of theme-1
+    | 'theme-2' // Description of theme-2
+    | 'theme-3' // Description of theme-3
+    | 'theme-4' // Description of theme-4
+    | 'theme-hero'; // hero theme
+}
+
+export {};
