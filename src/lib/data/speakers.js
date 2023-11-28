6405dbdeceb2395c71eb3dfc3d86636cc150bbb8
Fredrik Frodlund <fredrik@frodlund.se>
2023-11-28 17:54:26 +0100

    fix: add speaker photos & bios

--- src/lib/data/speakers.js
+++ src/lib/data/speakers.js
@@ -10,28 +10,33 @@
 const speakers = {
   'lisette-sutherland': {
     name: 'Lisette Sutherland',
-    image: '',
-    bio: ''
+    image: '/images/speakers/lisette-sutherland.jpg',
+    bio: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
   },
   'jonah-andersson': {
     name: 'Jonah Andersson',
-    image: '',
-    bio: ''
+    image: '/images/speakers/jonah-andersson.jpg',
+    bio: '<p>Coming soon…</p>'
   },
   'kesha-williams': {
     name: 'Kesha Williams',
-    image: '',
-    bio: ''
+    image: '/images/speakers/kesha-williams.jpg',
+    bio: '<p>Kesha Williams is an award-winning technology leader teaching others how to transform their lives through technology. She has 25+ years of experience architecting, designing, and building enterprise web applications. Her passions include teaching cloud topics and leading software engineering teams. Kesha holds multiple AWS certifications and is recognized as an AWS Machine Learning Hero, Alexa Champion, AWS Ambassador, and HackerRank All-Star. She currently serves as the Program Director of Slalom’s Cloud Residency and on the Board of Directors for Women in Voice.</p>'
   },
   'niall-merrigan': {
     name: 'Niall Merrigan',
-    image: '',
+    image: '/images/speakers/niall-merrigan.jpg',
     bio: '<p>Niall is an Irishman who is slowly being assimilated into Norwegian society. He works at Microsoft, help folks understand how to get the best from tech without going crazy. He really likes cooking and tends to talk obsessively about wine pairings.</p>'
   },
   'dylan-beattie': {
     name: 'Dylan Beattie',
-    image: '',
-    bio: ''
+    image: '/images/speakers/dylan-beattie.jpg',
+    bio: `
+      <p>Dylan Beattie is a consultant, software developer and international keynote speaker.
+      He’s been building web applications since the 1990s, and works primarily on Microsoft .NET, HTTP APIs, UX design, and distributed systems.</p>
+
+      <p>Dylan lives and works in London and when he’s not writing code he plays guitar and writes songs.</p>
+    `
   }
 };
 
