2c5eba0e5f53d7ae1e708d1d8bd9ff284d6abdfb
Fredrik Frodlund <fredrik@frodlund.se>
2023-12-02 21:20:00 +0100

    feat: add CTA to hero component

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,11 +1,16 @@
 <script>
   import CountDown from '$lib/components/CountDown.svelte';
+  import CallToAction from './CallToAction.svelte';
 </script>
 
 <div class="hero">
   <div class="hero__message">
     <p class="hero__time-date">March 2, 2024 — Malmö, Sweden</p>
     <CountDown endDate="2024/03/02 09:00:00" />
+    <CallToAction
+      text="Register now"
+      url="https://events.magnetevents.com/Event/beauty-in-code-2024-56171/"
+    />
     <p class="hero__price-tag">550 SEK <span class="hero__price-tag-vat">including VAT</span></p>
   </div>
 </div>
