d303d820b0c40de5feb027cee2dabfd445bf7a70
Fredrik Frodlund <fredrik@frodlund.se>
2023-12-04 09:41:36 +0100

    chore: update content for schedule

--- src/lib/data/schedule.js
+++ src/lib/data/schedule.js
@@ -28,8 +28,8 @@ const schedule = [
     end: '10:00',
     id: 'kesha-williams',
     name: 'Kesha Williams',
-    title: 'TBD',
-    description: `<p>Coming soon…</p>`
+    title: 'Ethics in AI',
+    description: `<p>This talk dives into the critical role of ethics in the fast-evolving AI landscape, with a special emphasis on Large Language Models (LLMs). We'll tackle the challenges and strategic solutions for ensuring data privacy, reducing biases, and enhancing transparency in AI systems. Anchored by real-world case studies, our discussion will underline the crucial balance between cutting-edge AI development and ethical responsibility. Participants will gain insights into responsibly implementing LLMs in data projects, focusing on compliance and ethical integrity.</p>`
   },
   {
     start: '10:00',
@@ -42,7 +42,7 @@ const schedule = [
     end: '11:15',
     id: 'niall-merrigan',
     name: 'Niall Merrigan',
-    title: 'TBD',
+    title: 'Optimistic Security',
     description: `<p>Coming soon…</p>`
   },
   {
@@ -50,8 +50,11 @@ const schedule = [
     end: '12:15',
     id: 'lisette-sutherland',
     name: 'Lisette Sutherland',
-    title: 'TBD',
-    description: `<p>Coming soon…</p>`
+    title: 'Thriving in the future of work: Embracing remote-first',
+    description: `
+      <p>Hybrid working existed before covid, but covid made it more widespread. With extensive polling of workers worldwide, the answer is clear: the future of work is choice. For organizations to survive and thrive in the modern world, we want to be able to work in a “remote first” way: working online as if we were in the office together.</p>
+    <p>In this presentation, tailored for workers who may think, "We've been working remotely for years now; what else is there to learn?" we'll dive deeper into the nuances of the virtual office and explore innovative strategies for seamless collaboration at a distance. We'll go beyond the basics and delve into advanced techniques digital nomads, virtual entrepreneurs, and global organizations use to foster a truly connected remote workforce.</p>
+    `
   },
   {
     start: '12:15',
@@ -64,16 +67,21 @@ const schedule = [
     end: '14:00',
     id: 'dylan-beattie',
     name: 'Dylan Beattie',
-    title: 'TBD',
-    description: `<p>Coming soon…</p>`
+    title: 'Analogue Evolution, Digital Revolution: Tipping Points in Technology',
+    description: `
+      <p>Technological progress is non-linear. Sometimes, innovation is a smooth curve; hundreds of small, incremental improvements over many years – until something comes along that changes the game; something that fundamentally challenges our assumptions around what technology can achieve. Within the last few decades, technology has profoundly and irreversibly changed the shape of human society; how we work, how we relax, how we communicate and collaborate. And, in almost every case, the key has been digitalisation: the ability to transform part of our reality into a stream of bits.</p>
+      <p>With the benefit of hindsight, it’s easy to see the tipping points, to identify the moments when a particular technology or idea achieved critical mass, when something went from being an interesting prototype to a viable product – but for people who were there at the time, it often wasn’t nearly so obvious. In an industry that’s perpetually excited about the “next big thing”, how do developers and technologists decide what to focus on? Will machine learning replace developers? Is AI a fun toy, a useful tool – or an existential threat to humanity? Join Dylan Beattie for an entertaining look at some of the innovations that really did change the world, and how understanding our history can help us make sense of the next digital revolution – whatever that turns out to be.</p>
+    `
   },
   {
     start: '14:15',
     end: '15:00',
     id: 'jonah-andersson',
     name: 'Jonah Andersson',
-    title: 'TBD',
-    description: `<p>Coming soon…</p>`
+    title: 'Cloud Migration and Modernization Mistakes You Should Avoid',
+    description: `
+      <p>Join Jonah Andersson, in this enlightening and lessons-learned session where she shares the landscape of cloud migration, her own cloud migration journey as a developer and IT architect. In this session, we will be exploring the most critical mistakes that developers, IT architects, and business leaders must avoid at all costs.</p>
+    `
   },
   {
     start: '15:00',
