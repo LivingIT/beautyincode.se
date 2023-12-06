98b217b9eefb33efbc349e399f7102f18e33a8e9
Fredrik Frodlund <fredrik@frodlund.se>
2023-12-06 17:48:01 +0100

    fix: missing fragment

---
+++ src/lib/fragments/analytics.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import config from '../../config';
+
+  $: {
+    if (typeof gtag !== 'undefined') {
+      gtag('config', config.analytics, {
+        page_title: document.title,
+        page_path: $page.url.pathname
+      });
+    }
+  }
+</script>
+
+<svelte:head>
+  {@html `<script async src="https://www.googletagmanager.com/gtag/js?id=${config.analytics}"}>
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', ${config.analytics});
+  </script>`}
+</svelte:head>
