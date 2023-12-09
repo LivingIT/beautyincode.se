70d1b5aee16a78b884f92f983fedc2e7239ffc64
Fredrik Frodlund <fredrik@frodlund.se>
2023-12-09 09:19:04 +0100

    feat: navigation improvements (#293)
    * chore: prettier code
    
    * feat: navigation improvements
    
    - make component more DRY
    - better SSR behavior
    - improve a11y
    
    * fix: make sure that aria-hidden is shown properly
    
    - remove attribute aria-hidden above 40em
    - add function to not run on SSR
    - make it reactive

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,17 +1,55 @@
 <script>
   import { onMount } from 'svelte';
   import { page } from '$app/stores';
+  import { browser } from '$app/environment';
 
   import Icon from './Icon.svelte';
+
+  const navItems = [
+    { text: 'Home', url: '/' },
+    { text: 'Speakers', url: '/speakers/' },
+    { text: 'Videos', url: '/videos/' }
+  ];
 
   /** @type {Boolean} */
   let isNavExpanded = false;
 
+  /** @type {Boolean | undefined} */
+  let ariaHidden;
+
   /** @type {HTMLElement} */
   let navTriggerElem;
 
+  /**
+   * Do not set aria-hidden on SSR or above 40em viewport
+   */
+  const updateAriaHidden = () => {
+    const isHorizontalNav = browser && window.matchMedia('(min-width: 40em)').matches;
+    ariaHidden = browser && !isHorizontalNav ? !isNavExpanded : undefined;
+  };
+
+  function toggleNav() {
+    isNavExpanded = !isNavExpanded;
+  }
+
+  /**
+   * Listen for tab input. If focus is outside mainNavElem, close it.
+   *
+   * @param {KeyboardEvent} e
+   */
+  const handleKeyUp = (e) => {
+    if (e.code === 'Tab') {
+      const mainNavElem = document.getElementById('main-nav');
+      if (!mainNavElem?.contains(document.activeElement)) {
+        isNavExpanded = false;
+      }
+    }
+  };
+
   onMount(() => {
+    updateAriaHidden();
     document.addEventListener('keyup', handleKeyUp);
+    window.addEventListener('resize', updateAriaHidden);
   });
 
   $: {
@@ -19,137 +57,137 @@
     isNavExpanded = false; // close nav when the route changes
   }
 
-  function toggleNav() {
-    isNavExpanded = !isNavExpanded;
-  }
-
-  /** @param {KeyboardEvent} e */
-  const handleKeyUp = (e) => {
-    if (e.code === 'Tab' && !navTriggerElem.contains(document.activeElement)) {
-      isNavExpanded = false;
-    }
-  };
+  $: updateAriaHidden();
 </script>
 
-<nav id="main-nav" aria-label="Main" data-expanded={isNavExpanded}>
-  <button
-    type="button"
-    id="main-nav-trigger"
-    aria-controls="main-nav"
-    aria-expanded={isNavExpanded}
-    aria-label="Menu: open main navigation"
-    on:click={toggleNav}
-    bind:this={navTriggerElem}
-  >
-    <Icon href="menu" />
-    <span>Menu</span>
-  </button>
-  <ul class="nav" aria-hidden={!isNavExpanded}>
-    <li class="nav__item"><a href="/">Home</a></li>
-    <li class="nav__item"><a href="/speakers/">Speakers</a></li>
-    <li class="nav__item"><a href="/videos/">Videos</a></li>
+<nav
+  id="main-nav"
+  class="main-nav"
+  class:main-nav--js={browser}
+  aria-label="Main"
+  data-expanded={isNavExpanded}
+>
+  {#if browser}
+    <button
+      type="button"
+      class="main-nav__trigger"
+      id="main-nav-trigger"
+      aria-controls="main-nav"
+      aria-expanded={isNavExpanded}
+      aria-label="Menu: open main navigation"
+      on:click={toggleNav}
+      bind:this={navTriggerElem}
+    >
+      <Icon href="menu" />
+      <span>Menu</span>
+    </button>
+  {/if}
+  <ul class="nav" aria-hidden={ariaHidden}>
+    {#each navItems as { text, url }}
+      <li class="nav__item"><a href={url} tabindex={isNavExpanded ? 0 : -1}>{text}</a></li>
+    {/each}
   </ul>
 </nav>
 
 <style>
-  #main-nav {
-    margin-top: 0;
-  }
-
-  #main-nav {
-    --nav-width: 12em;
-
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    width: var(--nav-width);
-    padding: 1em;
-    background-color: hsla(20, 10%, 18%, 1);
-    transition: right 200ms ease-in-out, filter 200ms ease-in-out;
-  }
-
-  #main-nav[data-expanded='true'] {
-    filter: drop-shadow(0 0 0.5em hsla(0, 0%, 0%, 0.5));
-  }
-
-  #main-nav[data-expanded='false'] {
-    right: calc(var(--nav-width) * -1);
-  }
-
-  /* Main navigation trigger */
-  #main-nav-trigger {
-    display: none;
-  }
-
-  #main-nav-trigger {
-    --width: 5.2em;
-    position: absolute;
-    top: 1em;
-    left: calc((var(--width) - 0.2em) * -1);
-    display: flex;
-    align-items: center;
-    padding: 0.5em 1em 0.5em 0.5em;
-    margin-top: 0;
-    font-size: 1.12em;
-    color: inherit;
-
-    background: hsl(20, 10%, 18%);
-    border: 0;
-  }
-
-  #main-nav-trigger span {
-    margin-inline-start: 0.25em;
-    margin-top: 0;
-  }
-
-  /* Navigation list */
-  .nav {
-    display: flex;
-    flex-direction: column;
-    padding: 0;
-    margin-top: 0;
-    font-weight: bold;
-    text-align: right;
-    text-transform: uppercase;
-  }
-
-  .nav__item a {
-    display: block;
-    padding: 0.25em 0;
-    text-decoration: none;
-  }
-
-  .nav__item a:hover,
-  .nav__item a:active {
-    text-decoration: underline;
-  }
-
-  @media (min-width: 40em) {
-    #main-nav {
-      --nav-width: unset;
-
-      position: static;
+  @media screen {
+    .main-nav {
+      margin-top: 0;
     }
 
-    #main-nav[data-expanded] {
-      filter: none;
+    .main-nav--js {
+      --nav-width: 12em;
+
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      width: var(--nav-width);
+      padding: 1em;
+      background-color: hsla(20, 10%, 18%, 1);
+      transition:
+        right 200ms ease-in-out,
+        filter 200ms ease-in-out;
     }
 
-    #main-nav-trigger {
-      display: none;
+    .main-nav--js[data-expanded='true'] {
+      filter: drop-shadow(0 0 0.5em hsla(0, 0%, 0%, 0.5));
     }
 
+    .main-nav--js[data-expanded='false'] {
+      right: calc(var(--nav-width) * -1);
+    }
+
+    .main-nav__trigger {
+      --width: 5.2em;
+
+      position: absolute;
+      top: 1em;
+      left: calc((var(--width) - 0.2em) * -1);
+      display: flex;
+      align-items: center;
+      padding: 0.5em 1em 0.5em 0.5em;
+      margin-top: 0;
+      font-size: 1.12em;
+      color: inherit;
+      cursor: pointer;
+      background: hsl(20, 10%, 18%);
+      border: 0;
+    }
+
+    .main-nav__trigger span {
+      margin-block-start: 0;
+      margin-inline-start: 0.25em;
+    }
+
+    /* Navigation list */
     .nav {
-      flex-direction: row;
+      display: flex;
+      flex-direction: column;
+      padding: 0;
+      margin-top: 0;
+      font-weight: bold;
+      text-align: right;
+      text-transform: uppercase;
     }
 
     .nav__item a {
-      padding: 0;
+      display: block;
+      padding: 0.25em 0;
+      text-decoration: none;
     }
 
-    .nav__item + .nav__item {
-      margin-left: 1em;
+    .nav__item a:hover,
+    .nav__item a:active {
+      text-decoration: underline;
+    }
+
+    @media (min-width: 40em) {
+      .main-nav--js {
+        --nav-width: unset;
+
+        position: static;
+      }
+
+      .main-nav--js[data-expanded] {
+        filter: none;
+      }
+
+      .main-nav__trigger {
+        display: none;
+      }
+
+      .nav {
+        flex-direction: row;
+      }
+
+      .nav__item a {
+        padding: 0;
+      }
+
+      .nav__item + .nav__item {
+        margin-left: 1em;
+      }
     }
   }
 </style>
