813a4bbd8eafc8bbae0e9045347b473105a1ae17
Fredrik Frodlund <fredrik@frodlund.se>
2024-01-10 09:59:23 +0100

    chore: refactor data for footer links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,33 +1,6 @@
 <script>
   import Icon from './Icon.svelte';
-
-  let footerLinks = [
-    {
-      label: 'E-mail',
-      link: 'mailto:beautyincode@livingit.se',
-      icon: 'email',
-    },
-    {
-      label: 'Twitter',
-      link: 'https://twitter.com/beautyincode',
-      icon: 'twitter',
-    },
-    {
-      label: 'Facebook',
-      link: 'https://www.facebook.com/beautyincode/',
-      icon: 'facebook',
-    },
-    {
-      label: 'LinkedIn',
-      link: 'https://www.linkedin.com/company/living-it',
-      icon: 'linkedin',
-    },
-    {
-      label: 'Instagram',
-      link: 'https://www.instagram.com/LivingITConsulting/',
-      icon: 'instagram',
-    },
-  ];
+  import { footerLinks } from '$lib/data/footerLinks';
 </script>
 
 <footer id="footer">
---
+++ src/lib/data/footerLinks.js
@@ -0,0 +1,27 @@
+export let footerLinks = [
+  {
+    label: 'E-mail',
+    link: 'mailto:beautyincode@livingit.se',
+    icon: 'email',
+  },
+  {
+    label: 'Twitter',
+    link: 'https://twitter.com/beautyincode',
+    icon: 'twitter',
+  },
+  {
+    label: 'Facebook',
+    link: 'https://www.facebook.com/beautyincode/',
+    icon: 'facebook',
+  },
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/company/living-it',
+    icon: 'linkedin',
+  },
+  {
+    label: 'Instagram',
+    link: 'https://www.instagram.com/LivingITConsulting/',
+    icon: 'instagram',
+  },
+];
