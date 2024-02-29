c1ea5633564745b3b09e33da9b8f9b0ba73b5ba8
Fredrik Frodlund <fredrik@frodlund.se>
2024-02-29 19:22:35 +0100

    chore: hide CallToAction and display closed message (#380)

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,17 +1,20 @@
 <script>
   import CountDown from '$lib/components/CountDown.svelte';
-  import CallToAction from './CallToAction.svelte';
+  // import CallToAction from './CallToAction.svelte';
 </script>
 
 <div class="hero">
   <div class="hero__message">
     <p class="hero__time-date">March 2, 2024 — Malmö, Sweden</p>
     <CountDown endDate="2024/03/02 09:00:00" />
+    <!--
     <CallToAction
       text="Register now"
       url="https://events.magnetevents.com/Event/beauty-in-code-2024-56171/"
     />
-    <p class="hero__price-tag">550 SEK <span class="hero__price-tag-vat">including VAT</span></p>
+    -->
+    <p class="hero__closed">Ticket sales are now closed! See you on Saturday!</p>
+    <!-- <p class="hero__price-tag">550 SEK <span class="hero__price-tag-vat">including VAT</span></p> -->
   </div>
 </div>
 
@@ -52,6 +55,11 @@
     display: block;
     font-size: 0.5em;
     font-weight: normal;
+  }
+
+  .hero__closed {
+    font-size: 1.1em;
+    font-weight: bold;
   }
 
   @media (min-width: 30em) {
