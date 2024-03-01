ab6d29959f42c021a5d0d265ee7bd55a86c858dd
Fredrik Frodlund <fredrik@frodlund.se>
2024-03-01 08:48:16 +0100

    feat: hide largest > 0 unit (#381)
    * fix: remove zero padding from countdown
    
    * feat: hide largest unit if > 0

--- src/lib/components/CountDown.svelte
+++ src/lib/components/CountDown.svelte
@@ -54,11 +54,18 @@
 
 <p id="countdown" class="countdown">
   {#if browser}
-    <span class="at-only">Event begins in</span>
-    <span class="countdown__unit days">{days}</span>
-    <span class="countdown__unit hours">{hours < 10 ? '0' + hours : hours}</span>
-    <span class="countdown__unit minutes">{minutes < 10 ? '0' + minutes : minutes}</span>
-    <span class="countdown__unit seconds">{seconds < 10 ? '0' + seconds : seconds}</span>
+    {#if days > 0}
+      <span class="countdown__unit days">{days}</span>
+    {/if}
+    {#if hours > 0 || days > 0}
+      <span class="countdown__unit hours">{hours}</span>
+    {/if}
+    {#if minutes > 0 || hours > 0 || days > 0}
+      <span class="countdown__unit minutes">{minutes}</span>
+    {/if}
+    {#if seconds > 0 || minutes > 0 || hours > 0 || days > 0}
+      <span class="countdown__unit seconds">{seconds}</span>
+    {/if}
   {:else}
     <span class="countdown__ssr">
       Doors open in <em class="countdown__ssr-days">{days}</em> days
