3a99e4219d8f5ca7160622a0170a250b24b9989d
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-01 17:46:01 +0100

    hack: rename

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Footer from '$lib/components/footer.svelte';
-  import Header from '$lib/components/header.svelte';
+  import Header from '$lib/components/header_.svelte';
   import {
     metaDescription,
     googleSiteVerification,
