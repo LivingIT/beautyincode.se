5c168e3e309e9c91fbd979d38c9ba5b93d2a8bf5
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-01 17:21:03 +0100

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	server: {
-		host: '0.0.0.0',
-		port: 9002
-	},
-	css: {
-		devSourcemap: true
-	},
-	plugins: [sveltekit()]
+  server: {
+    host: '0.0.0.0',
+    port: 9001,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [sveltekit()],
 });
