01356082b0676d1b5e97e6b37577d9159b3d1140
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-01 18:24:28 +0100

    hack: deployTime

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ import {
   PUBLIC_GOOGLE_SITE_VERIFICATION,
   PUBLIC_META_DESCRIPTION,
   PUBLIC_OG_IMAGE,
-  PUBLIC_DEPLOY_TIMESTAMP,
+  // PUBLIC_DEPLOY_TIMESTAMP,
 } from '$env/static/public';
 
 export const baseUrl = PUBLIC_BASE_URL;
@@ -12,4 +12,4 @@ export const title = PUBLIC_SITE_TITLE;
 export const googleSiteVerification = PUBLIC_GOOGLE_SITE_VERIFICATION;
 export const metaDescription = PUBLIC_META_DESCRIPTION;
 export const ogImage = PUBLIC_OG_IMAGE;
-export const deployTime = PUBLIC_DEPLOY_TIMESTAMP || '';
+export const deployTime = '';
