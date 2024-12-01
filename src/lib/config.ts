65b8626bb0514db9f564d4aa1d3e188888400352
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-01 18:58:35 +0100

    fix: revert

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,13 +5,11 @@ import {
   PUBLIC_META_DESCRIPTION,
   PUBLIC_OG_IMAGE,
   // PUBLIC_DEPLOY_TIMESTAMP,
-  CF_PAGES_URL,
 } from '$env/static/public';
 
-export const baseUrl = CF_PAGES_URL || PUBLIC_BASE_URL;
+export const baseUrl = PUBLIC_BASE_URL;
 export const title = PUBLIC_SITE_TITLE;
 export const googleSiteVerification = PUBLIC_GOOGLE_SITE_VERIFICATION;
 export const metaDescription = PUBLIC_META_DESCRIPTION;
 export const ogImage = PUBLIC_OG_IMAGE;
 export const deployTime = '';
-export const cfPagesURL = CF_PAGES_URL;
