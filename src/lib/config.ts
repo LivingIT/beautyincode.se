0a1dadc22d119d0eccef40cf864785c8ececfad0
Fredrik Frodlund <fredrik@frodlund.se>
2024-12-03 10:55:56 +0100

    test: use CF_PAGES_URL directly

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,4 @@
 import {
-  PUBLIC_BASE_URL,
   PUBLIC_SITE_TITLE,
   PUBLIC_GOOGLE_SITE_VERIFICATION,
   PUBLIC_META_DESCRIPTION,
@@ -8,9 +7,7 @@ import {
   PUBLIC_DEPLOY_TIMESTAMP,
 } from '$env/static/public';
 
-const cfPagesUrl = import.meta.env.VITE_CF_PAGES_URL || process.env.CF_PAGES_URL;
-
-export const baseUrl = cfPagesUrl || PUBLIC_BASE_URL; // Use Cloudflare's URL if available
+export const baseUrl = process.env.CF_PAGES_URL;
 export const title = PUBLIC_SITE_TITLE;
 export const googleSiteVerification = PUBLIC_GOOGLE_SITE_VERIFICATION;
 export const metaDescription = PUBLIC_META_DESCRIPTION;
