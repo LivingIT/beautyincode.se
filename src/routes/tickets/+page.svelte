7048d974dacb911b61146481c55cb48c3c4a9651
Fredrik Frodlund <fredrik@frodlund.se>
2025-12-19 09:02:15 +0100

    feat: update tickets page

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { title } from '$lib/config';
+  import { eventUrl, title } from '$lib/config';
 
-  // import ArticleSection from '$lib/components/articleSection.svelte';
+  import ArticleSection from '$lib/components/articleSection.svelte';
   import ParallaxSection from '$lib/components/parallaxSection.svelte';
+  import ButtonCTA from '$lib/components/buttonCTA.svelte';
 
   const pageTitle = 'Tickets';
 </script>
@@ -11,11 +12,24 @@
   <title>{pageTitle} - {title}</title>
 </svelte:head>
 
-<ParallaxSection backgroundImage="/images/covers/tickets.webp" isSemi={false}>
-  <h1>Ticket sales for 2026 will open soon!</h1>
+<ParallaxSection backgroundImage="/images/covers/tickets.webp" isSemi={true}>
+  <h1>Tickets for Beauty in Code 2026</h1>
 </ParallaxSection>
 
-<!-- <ArticleSection> -->
-<!--   <h2>Ticket sales have closed!</h2> -->
-<!--   <p>See you all on Saturday!</p> -->
-<!-- </ArticleSection> -->
+<ArticleSection>
+  <p>
+    Beauty in Code aims to keep conference attendance accessible, with ticket prices reflecting only
+    the actual per-attendee costs, including venue and catering.
+  </p>
+
+  <aside>
+    <ButtonCTA url={eventUrl} text="Get tickets now" />
+  </aside>
+</ArticleSection>
+
+<style>
+  aside {
+    margin-block: 4rem;
+    text-align: center;
+  }
+</style>
