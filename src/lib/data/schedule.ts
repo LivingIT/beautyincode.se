fca1e8251af31a63bb07b44ad886c9717e5dd51e
Fredrik Frodlund <fredrik@frodlund.se>
2026-01-07 11:17:18 +0100

    chore: add brief for Dylan

--- src/lib/data/schedule.ts
+++ src/lib/data/schedule.ts
@@ -57,8 +57,12 @@ export const schedule: Array<ScheduleItem> = [
     time: '13:15',
     speakerSlug: 'dylan-beattie',
     name: 'Dylan Beattie',
-    title: 'TBA',
-    description: `<p>TBA!</p>`,
+    title: 'Do It With Style: Rethinking CSS',
+    description: `<p>Cascading Style Sheets. CSS. The language everybody loves to hate. Everything's global, there's no control flow, there's no error messages... in fact, some people say it isn't even a real programming language.</p>
+
+<p>But CSS isn't just about making websites look pretty. It's one of the fundamental building blocks of the open web, and it's continuously evolving. Modern CSS has variables, grids, flexboxes, animations, view transitions... When it comes to layout, accessibility, interaction and validation, CSS can do all kinds of things you thought were impossible - or at least, only possible with a lot of complicated JavaScript. Dylan Beattie's been building web applications since before CSS was invented - yes, really - and in this session, he'll take you on a whirlwind tour of some of the weird and wonderful things you can do in 2026 using semantic HTML, a generous sprinkling of CSS, and absolutely no JavaScript.</p>
+
+<p>OK, maybe a tiny bit of JavaScript. But absolutely no React.</p>`,
   },
   {
     time: '14:15',
